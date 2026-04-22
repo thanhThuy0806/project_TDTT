6f02efc920d567ba9c6dd7f3f645b74fa94c458b
thanhThuy0806 <tieuso2515@gmail.com>
2026-04-23 00:58:22 +0700

    [add] cron.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
+import { eq, and } from "drizzle-orm";
+import job from "./config/cron.js";
 
 const app = express();
 const PORT = ENV.PORT || 5001;
+
+if (ENV.NODE_ENV === "production") job.start();
 
 app.use(express.json());
 
