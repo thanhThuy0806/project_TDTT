447cde0b4a987bf5782d5fc799d491435cdf2c18
thanhThuy0806 <tieuso2515@gmail.com>
2026-04-23 00:47:03 +0700

    Fix: add .js extension to env import

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { ENV } from "./env";
-import * as schema from "../db/schema";
+import { ENV } from "./env.js";
+import * as schema from "../db/schema.js";
 
 const sql = neon(ENV.DATEBASE_URL);
 export const db = drizzle(sql, { schema });
