2f924092df48deb917aae0e33c505f911433d6bf
thanhThuy0806 <tieuso2515@gmail.com>
2026-04-24 22:06:46 +0700

    [change] firebase.js (24/4/26)

--- frontend/firebase/firebase.js
+++ frontend/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9RSeXcAUhkq6Z_AWzy2iKj3GYZgxwhWQ",
-  authDomain: "projecttdtt-de605.firebaseapp.com",
-  projectId: "projecttdtt-de605",
-  storageBucket: "projecttdtt-de605.firebasestorage.app",
-  messagingSenderId: "155544973531",
-  appId: "1:155544973531:web:f0e0fe07b4241f39f09cfe",
-  measurementId: "G-1V8T8X7GMN",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
